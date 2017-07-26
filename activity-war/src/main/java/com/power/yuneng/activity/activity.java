fe3afb376d8ef23f06a943bb29645323188048bd
wuhun0301 <wuhun0301@qq.com>
2017-07-26 10:05:05 +0800

    init

---
+++ activity-war/src/main/java/com/power/yuneng/activity/activity.java
@@ -0,0 +1,13 @@
+package com.power.yuneng.activity;
+
+import org.springframework.context.annotation.ImportResource;
+
+/**
+ * Created by Administrator on 2017/7/26.
+ */
+@ImportResource(locations = {"classpath*:config/zgk-dubbo-consumer.xml"})
+public class activity {
+    public static void main(String[] args) {
+
+    }
+}
